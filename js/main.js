263344bb5eec44e063def18cbf802cb6a8d5f01a
Ciro9320 <ciropassata93@gmail.com>
2023-12-02 16:13:35 +0100

    creato la pagina relativa al glossario

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 menuIsOpen = false;
 const m = document.getElementById("mobile");
+const g = document.getElementById("glossario-main");
 const b = document.getElementById("back-to-top");
+const bg = document.getElementById("back-to-top-glossario");
+
+const k = document.getElementsByClassName("keyword");
+const s = document.getElementById("search-input");
 
 function menuToggle() {
     if (!menuIsOpen) {
@@ -32,5 +37,48 @@ window.addEventListener("scroll", () => {
         b.classList.remove("opacity-100");
         b.classList.add("invisible");
         b.classList.add("opacity-0");
+    }
+});
+
+function backToTopGlossario() {
+    g.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+}
+
+g.addEventListener("scroll", () => {
+    if (g.scrollTop > 300) {
+        bg.classList.remove("invisible");
+        bg.classList.remove("opacity-0");
+        bg.classList.add("visible");
+        bg.classList.add("opacity-100");
+    } else {
+        bg.classList.remove("visible");
+        bg.classList.remove("opacity-100");
+        bg.classList.add("invisible");
+        bg.classList.add("opacity-0");
+    }
+});
+
+s.addEventListener("keyup", () => {
+    let p = s.value.toLowerCase();
+    let l = p.length;
+    var i = 0;
+    for (; i < k.length; i++) {
+        let key = k[i].innerHTML;
+        if (!p.trim()) {
+            return;
+        }
+        key = key.toLowerCase();
+        key = key.trim();
+        key = key.substring(0, l);
+        if (key == p) {
+            k[i].scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+            break;
+        }
     }
 });
